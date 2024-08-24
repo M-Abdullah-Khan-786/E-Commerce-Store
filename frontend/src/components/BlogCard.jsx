0d3252c435e04bb98ab2efebc1f6ddf00975b629
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-08-24 11:30:12 +0500

    Initial commit

---
+++ frontend/src/components/BlogCard.jsx
@@ -0,0 +1,29 @@
+import "../CSS/BlogCard.css";
+import { Link } from "react-router-dom";
+import blog from "../assets/blog.jpeg";
+
+const BlogCard = () => {
+  return (
+    <>
+      <div className="col-3">
+        <div className="blog-card">
+          <div className="blog-img">
+            <img src={blog} alt="Blog Image" className="img-fluid" />
+          </div>
+          <div className="blog-content">
+            <p className="date">22 August 2024</p>
+            <h5 className="title">My Blog</h5>
+            <p className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iure
+              pariatur non minima, iste id, ut repellendus possimus, placeat
+              nostrum sit. Vel!
+            </p>
+            <Link to="/" className="buttton text-decoration-none">Read more</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogCard;
