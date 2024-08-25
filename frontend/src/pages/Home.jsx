68db974eed49d54caad7f400561d66fd659d32f5
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-08-25 13:37:21 +0500

    Initial commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,12 +9,8 @@ import { RiDiscountPercentFill } from "react-icons/ri";
 import { FaRegCreditCard } from "react-icons/fa";
 import camera from "../assets/camera.jpg";
 import brand1 from "../assets/brand1.png";
-import brand2 from "../assets/brand2.png";
-import brand3 from "../assets/brand3.png";
-import brand4 from "../assets/brand4.png";
-import brand5 from "../assets/brand5.png";
-import brand6 from "../assets/brand6.png";
 import BlogCard from "../components/BlogCard";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   return (
@@ -203,23 +199,44 @@ const Home = () => {
                     <img src={brand1} alt="brand-Images" className="img-fluid" />
                   </div>
                   <div className="mx-4 w-25">
-                    <img src={brand2} alt="brand-Images" className="img-fluid" />
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
                   </div>
                   <div className="mx-4 w-25">
-                    <img src={brand3} alt="brand-Images" className="img-fluid" />
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
                   </div>
                   <div className="mx-4 w-25">
-                    <img src={brand4} alt="brand-Images" className="img-fluid" />
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
                   </div>
                   <div className="mx-4 w-25">
-                    <img src={brand5} alt="brand-Images" className="img-fluid" />
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
                   </div>
                   <div className="mx-4 w-25">
-                    <img src={brand6} alt="brand-Images" className="img-fluid" />
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src={brand1} alt="brand-Images" className="img-fluid" />
                   </div>
                 </Marquee>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="feature-wrapper home-wrapper-2 py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">
+                Featured Collections
+              </h3>
+            </div>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
           </div>
         </div>
       </section>
