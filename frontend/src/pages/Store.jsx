3ce238815216385d1fcc936705adab83d0aa6727
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-08-29 12:12:16 +0500

    Initial commit

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,6 +1,8 @@
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import "../CSS/Store.css";
+import ReactStars from "react-rating-stars-component";
+import product from "../assets/product.png";
 
 const Store = () => {
   return (
@@ -26,15 +28,90 @@ const Store = () => {
                 <h3 className="filter-title">Filter By</h3>
                 <div>
                   <h5 className="sub-title">Availability</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        In Stock (22)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        Out of Stock (2)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <h5 className="sub-title">Price</h5>
+                <div className="d-flex align-items-center gap-10">
+                  <div className="form-floating">
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="From"
+                    />
+                    <label htmlFor="floatingInput">From</label>
+                  </div>
+                  <div className="form-floating">
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="floatingInput1"
+                      placeholder="To"
+                    />
+                    <label htmlFor="floatingInput1">To</label>
+                  </div>
+                </div>
+                <h5 className="sub-title">Colors</h5>
+                <div>
+                  <ul className="colors ps-0">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+                <h5 className="sub-title">Size</h5>
+                <div>
                   <div className="form-check">
                     <input
                       className="form-check-input"
                       type="checkbox"
                       value=""
-                      id=""
+                      id="size-1"
                     />
-                    <label className="form-check-label" htmlFor="">
-                      In Stock (22)
+                    <label className="form-check-label" htmlFor="size-1">
+                      SM
                     </label>
                   </div>
                   <div className="form-check">
@@ -42,28 +119,103 @@ const Store = () => {
                       className="form-check-input"
                       type="checkbox"
                       value=""
-                      id=""
+                      id="size-2"
                     />
-                    <label className="form-check-label" htmlFor="">
-                      Out of Stock (2)
+                    <label className="form-check-label" htmlFor="size-2">
+                      MD
                     </label>
                   </div>
-                </div>
-                <div>
-                  <h5 className="sub-title"></h5>
-                </div>
-                <div>
-                  <h5 className="sub-title"></h5>
-                </div>
-                <div>
-                  <h5 className="sub-title"></h5>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="size-3"
+                    />
+                    <label className="form-check-label" htmlFor="size-3">
+                      XL
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="size-4"
+                    />
+                    <label className="form-check-label" htmlFor="size-4">
+                      XXL
+                    </label>
+                  </div>
                 </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Product Tags</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Speakers
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Charger
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Light
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Watch
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Glasses
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Staionary
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Books
+                    </span>
+                  </div>
+                </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Products</h3>
+                <div>
+                  <div className="random-products py-3 d-flex">
+                    <div className="w-50">
+                      <img src={product} alt="Product" className="img-fluid" />
+                    </div>
+                    <div className="w-50">
+                      <h5>Wireless Headphones</h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value="5"
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>Rs 2200</b>
+                    </div>
+                  </div>
+                  <div className="random-products py-3 d-flex">
+                    <div className="w-50">
+                      <img src={product} alt="Product" className="img-fluid" />
+                    </div>
+                    <div className="w-50">
+                      <h5>Wireless Headphones</h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value="5"
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>Rs 2200</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="col-9">main</div>
