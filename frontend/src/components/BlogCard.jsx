4469e45885c4733793bf5050cc0fd2b3147ce766
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-08-31 13:04:01 +0500

    Initial commit

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -5,23 +5,21 @@ import blog from "../assets/blog.jpeg";
 const BlogCard = () => {
   return (
     <>
-      <div className="col-3 mb-3">
-        <div className="blog-card">
-          <div className="blog-img">
-            <img src={blog} alt="Blog Image" className="img-fluid" />
-          </div>
-          <div className="blog-content">
-            <p className="date">22 August 2024</p>
-            <h5 className="title">My Blog</h5>
-            <p className="desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iure
-              pariatur non minima, iste id, ut repellendus possimus, placeat
-              nostrum sit. Vel!
-            </p>
-            <Link to="/" className="button text-decoration-none">
-              Read more
-            </Link>
-          </div>
+      <div className="blog-card">
+        <div className="blog-img">
+          <img src={blog} alt="Blog Image" className="img-fluid w-100" />
+        </div>
+        <div className="blog-content">
+          <p className="date">22 August 2024</p>
+          <h5 className="title">My Blog</h5>
+          <p className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iure
+            pariatur non minima, iste id, ut repellendus possimus, placeat
+            nostrum sit. Vel!
+          </p>
+          <Link to="/" className="button text-decoration-none">
+            Read more
+          </Link>
         </div>
       </div>
     </>
