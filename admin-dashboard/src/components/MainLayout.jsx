7b3a882a97f6bf918f09d9fd5d630119e6c93ae3
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-09-21 10:31:19 +0500

    Initial commit

--- admin-dashboard/src/components/MainLayout.jsx
+++ admin-dashboard/src/components/MainLayout.jsx
@@ -3,14 +3,16 @@ import { useState } from "react";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Button, Layout, Menu, theme } from "antd";
 const { Header, Sider, Content } = Layout;
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { TbDashboard } from "react-icons/tb";
 import { FaRegUserCircle } from "react-icons/fa";
-import { GrCatalog } from "react-icons/gr";
+import { GrCatalog, GrDeliver } from "react-icons/gr";
 import { FaProductHunt } from "react-icons/fa6";
 import { SiBrandfolder } from "react-icons/si";
 import { MdCategory } from "react-icons/md";
 import { AiOutlineBgColors } from "react-icons/ai";
+import { ImBlogger } from "react-icons/im";
+import { MdOutlineContactPhone } from "react-icons/md";
 
 const MainLayout = () => {
   const navigate = useNavigate();
@@ -21,7 +23,9 @@ const MainLayout = () => {
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
+        <div className="demo-logo-vertical">
+          <h2 className="text-center text-white py-3 fs-5 mb-0">E-Store</h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
@@ -48,7 +52,7 @@ const MainLayout = () => {
               key: "catalog",
               icon: <GrCatalog className="fs-4" />,
               label: "Catalog",
-              children:[
+              children: [
                 {
                   key: "product",
                   icon: <FaProductHunt className="fs-4" />,
@@ -89,13 +93,51 @@ const MainLayout = () => {
                   icon: <MdCategory className="fs-4" />,
                   label: "Category List",
                 },
-              ]
+              ],
+            },
+            {
+              key: "orders",
+              icon: <GrDeliver className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blogs",
+              icon: <ImBlogger className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blog",
+                  icon: <ImBlogger className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blogs-list",
+                  icon: <ImBlogger className="fs-4" />,
+                  label: "Blog List",
+                },
+                {
+                  key: "blog-category",
+                  icon: <ImBlogger className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blogs-category-list",
+                  icon: <ImBlogger className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "inquiries",
+              icon: <MdOutlineContactPhone className="fs-4" />,
+              label: "Inquiries",
             },
           ]}
         />
       </Sider>
       <Layout>
         <Header
+        className="d-flex justify-content-between ps-3 pe-5"
           style={{
             padding: 0,
             background: colorBgContainer,
@@ -111,6 +153,9 @@ const MainLayout = () => {
               height: 64,
             }}
           />
+          <div className="d-flex gap-3 align-items-center" >
+            <h1>Admin</h1>
+          </div>
         </Header>
         <Content
           style={{
@@ -121,7 +166,7 @@ const MainLayout = () => {
             borderRadius: borderRadiusLG,
           }}
         >
-          Content
+          <Outlet/>
         </Content>
       </Layout>
     </Layout>
