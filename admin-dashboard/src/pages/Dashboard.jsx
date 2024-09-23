bb3c2f3ca12ea02a60ec21e0f43badc1f0c22562
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-09-23 14:57:40 +0500

    Initial commit

--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -1,9 +1,47 @@
+import "../CSS/Dashboard.css";
+import { FiArrowDownRight } from "react-icons/fi";
+
 const Dashboard = () => {
   return (
     <>
-      Dashboard
+      <h3 className="mb-4">Dashboard</h3>
+      <div className="d-flex justify-content-between align-items-center gap-3">
+        <div className="d-flex justify-content-between align-items-center flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p> <h4 className="mb-0">Rs 2200</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6>
+              <FiArrowDownRight /> 22%
+            </h6>
+            <p className="mb-0">Compare to Last Month</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-center flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p> <h4 className="mb-0">Rs 2200</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <FiArrowDownRight /> 22%
+            </h6>
+            <p className="mb-0">Compare to Last Month</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-center flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p> <h4 className="mb-0">Rs 2200</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <FiArrowDownRight /> 22%
+            </h6>
+            <p className="mb-0">Compare to Last Month</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
