e6d08f4fd349304c6970cd483dce52e20971b907
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-10-03 20:00:36 +0500

    Initial commit

--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -76,40 +76,40 @@ const Dashboard = () => {
       <div className="d-flex justify-content-between align-items-center gap-3">
         <div className="d-flex justify-content-between align-items-center flex-grow-1 bg-white p-3 rounded-3">
           <div>
-            <p className="mb-0">Total</p> <h4 className="mb-0">Rs 2200</h4>
+            <p className="mb-0 content">Total</p> <h4 className="mb-0 sub-title">Rs 2200</h4>
           </div>
           <div className="d-flex flex-column align-items-end">
-            <h6>
+            <h6 className="black">
               <FiArrowDownRight /> 22%
             </h6>
-            <p className="mb-0">Compare to Last Month</p>
+            <p className="mb-0 content">Compare to Last Month</p>
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-center flex-grow-1 bg-white p-3 rounded-3">
           <div>
-            <p className="mb-0">Total</p> <h4 className="mb-0">Rs 2200</h4>
+            <p className="mb-0 content">Total</p> <h4 className="mb-0 sub-title">Rs 2200</h4>
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6 className="red">
               <FiArrowDownRight /> 22%
             </h6>
-            <p className="mb-0">Compare to Last Month</p>
+            <p className="mb-0 content">Compare to Last Month</p>
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-center flex-grow-1 bg-white p-3 rounded-3">
           <div>
-            <p className="mb-0">Total</p> <h4 className="mb-0">Rs 2200</h4>
+            <p className="mb-0 content">Total</p> <h4 className="mb-0 sub-title">Rs 2200</h4>
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6 className="green">
               <FiArrowDownRight /> 22%
             </h6>
-            <p className="mb-0">Compare to Last Month</p>
+            <p className="mb-0 content">Compare to Last Month</p>
           </div>
         </div>
       </div>
       <div className="mt-4">
-        <h3 className="mb-4">Income Stats</h3>
+        <h3 className="mb-4 content">Income Stats</h3>
         <div>
           <Column {...config} />
         </div>
