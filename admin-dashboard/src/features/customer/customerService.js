d571ebb0b839266c8b762116b352f0fcf6472c2a
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-11-21 19:42:48 +0500

    Initial commit

--- admin-dashboard/src/features/customer/customerService.js
+++ admin-dashboard/src/features/customer/customerService.js
@@ -1,14 +1,8 @@
 import axios from "axios"
 import { base_url } from "../../utils/base_url"
 
-const getUsers = async()=>{
+export const getUser = async()=>{
     const response = await axios.get(`${base_url}/user/all-users`)
     return response.data
 }
-
-const customer = {
-    getUsers
-}
-
-export default customer;
 
